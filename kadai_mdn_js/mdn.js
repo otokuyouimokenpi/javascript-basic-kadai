1d1e5e68c682cc008709aa8c1288509c21397831
otokuyouimokenpi <mksr74dc@gmail.com>
2024-08-22 19:36:47 +0900

    mdn_js課題追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+let td = new Date();
+
+let year = td.getFullYear();
+
+let month = td.getMonth() + 1;
+
+let date = td.getDate();
+
+const today = year + '年' + month + '月' + date + '日';
+
+console.log(today);
